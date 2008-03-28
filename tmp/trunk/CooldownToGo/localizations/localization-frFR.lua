34a568c74d3f890b16b6ce8d312a76e3fc8c9905
mitchell <mitchell@81afdd8c-566f-49d0-a6b8-c6915fddcf4b>
2008-03-28 10:05:44 +0000

    CooldownToGo: localizations

---
+++ tmp/trunk/CooldownToGo/localizations/localization-frFR.lua
@@ -0,0 +1,25 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("CooldownToGo", "frFR", false)
+if not L then return end
+		
+-- frFR by |Pixel|
+L["Locked"] = "Vérouillé"
+L["Lock/Unlock display frame"] = "(Dé)Vérouille la fenêtre"
+-- L["Hold time"] = true
+-- L["Time to hold the message in seconds"] = true
+L["Font"] = "Police"
+L["Default"] = "Défaut"
+L["Font size"] = "Taille de police"
+L["Font outline"] = "Epaisseur de police"
+L["None"] = "Aucun"
+L["Normal"] = "Normal"
+L["Thick"] = "Epais"
+L["Color"] = "Couleur"
+L["%s loaded. Type /cdtg for help"] = "%s chargé. Tapez /cdtg pour obtenir de l'aide"
+L["Strata"] = "Profondeur"
+L["Frame strata"] = "Profondeur de la fenêtre"
+L["High"] = "Au dessus"
+L["Medium"] = "Normal"
+L["Low"] = "En dessous"
+-- L["Configure"] = true
+-- L["Bring up GUI configure dialog"] = true
+
---
+++ tmp/trunk/CooldownToGo/localizations/localization-koKR.lua
@@ -0,0 +1,24 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("CooldownToGo", "koKR", false)
+if not L then return end
+		
+L["Locked"] = "잠금"
+L["Lock/Unlock display frame"] = "표시 창을 고정시키거나 이동시킵니다."
+-- L["Hold time"] = true
+-- L["Time to hold the message in seconds"] = true
+L["Font"] = "글꼴"
+L["Default"] = "기본값"
+L["Font size"] = "글꼴 크기"
+L["Font outline"] = "글꼴 윤각"
+L["None"] = "없음"
+L["Normal"] = "보통"
+L["Thick"] = "굵게"
+L["Color"] = "색상"
+L["%s loaded. Type /cdtg for help"] = "%s 불려옵니다. 도움말을 보실려면 /cdtg 를 입력하세요."
+L["Strata"] = "우선순위"
+L["Frame strata"] = "창 우선순위"
+L["High"] = "높음"
+L["Medium"] = "중간"
+L["Low"] = "낮음"
+-- L["Configure"] = true
+-- L["Bring up GUI configure dialog"] = true
+
