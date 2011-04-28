9a72893334f0dc2e34efc37f4a4b32b03e782b94
mitch0 <mitch0@81afdd8c-566f-49d0-a6b8-c6915fddcf4b>
2011-04-28 19:17:35 +0000

    Tagging as v1.9.0

---
+++ tags/v1.9.0/CooldownToGo_Options/localizations/zhCN.lua
@@ -0,0 +1,8 @@
+-- This file is automagically generated.
+-- Please visit http://www.wowace.com/projects/cooldown-to-go/localization/
+-- if you wish to help with the translation.
+
+local L = LibStub("AceLocale-3.0"):NewLocale("CooldownToGo_Options", "zhCN")
+if not L then return end
+
+--@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="none", namespace="options", handle-unlocalized="ignore")@
