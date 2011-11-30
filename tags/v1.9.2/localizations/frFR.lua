950eb8dca78bc0207e94852da20213f5ed749b2f
mitch0 <mitch0@81afdd8c-566f-49d0-a6b8-c6915fddcf4b>
2011-11-30 12:09:20 +0000

    Tagging as v1.9.2

---
+++ tags/v1.9.2/localizations/frFR.lua
@@ -0,0 +1,8 @@
+-- This file is automagically generated.
+-- Please visit http://www.wowace.com/projects/cooldown-to-go/localization/
+-- if you wish to help with the translation.
+
+local L = LibStub("AceLocale-3.0"):NewLocale("CooldownToGo", "frFR")
+if not L then return end
+
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
